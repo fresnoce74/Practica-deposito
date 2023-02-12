9c1212f65951711983fcd15e7f14c95bd40c1ee1
Cecilio Fresno <ceciliofresno@gmail.com>
2023-02-12 11:33:50 -0600

    Modificación proyecto

--- CCuenta.java
+++ CCuenta.java
@@ -1,21 +1,46 @@
-package cuentas;
 
+
+
+package cuentas;
+/**
+ * Ejercicio de entorno de desarrollo para la practica de refactorizar y crear un documento
+ * detallado de la alicación.Esta clase es la que interactua con el metodo operativa_cuenta().                                                           
+ * @author cecilio fFresno.   
  */
+
+
+public class CCuenta {
+	
 
 /**
- *
- * @author cecil
+ * Atirbutos de la clase.Aquí se puede ver la encapsulación. 
  */
-public class CCuenta {
-
-    private String nombre;
+	private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta(){
+    
+    /**
+     * Constructor vacio sin parametro.Para inicializar sin ningún parametro.
+     */
+   
+    public CCuenta()
+    {
+
+   
+
     }
 
+  
+    /**
+     * Constructor con 4 parametros para su inicialización.
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+  
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -23,28 +48,131 @@ public class CCuenta {
         saldo=sal;
     }
 
+/**
+ *  Metodo que  devuelve el nombre que nos han dado.
+ * Aquí se puede ver la encapsulación.  
+ * @return
+ */
+
+
+
+ 
+    public String getNombre() {
+		return nombre;
+	}
+
+/**
+ * Metodo que nos pide un parametro(nombre),pero no devuelve nada.
+ * Aquí se puede ver la encapsulación. 
+ * @param nombre
+ */
+    public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+/**
+ *Metodo que  devuelve el parametro cuenta que nos han dado.
+ *Aquí se puede ver la encapsulación.  
+ * @return
+ */
+
+    public String getCuenta() {
+		return cuenta;
+	}
+
+/**
+ * Metodo que nos pide un parametro(cuenta),pero no devuelve nada.
+ * Aquí se puede ver la encapsulación. 
+ * @param cuenta
+ */
+    
+    public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	
+/**
+ * Metodo que nos devulve el saldo de tipo double.
+ * Aquí se puede ver la encapsulación.  
+ * @return
+ */
+  
+  
+  
+    public double getSaldo() {
+		return saldo;
+	}
+
+	
+    /**
+     * Metodo que nos pide el saldo de la cuenta.
+     * Aquí se puede ver la encapsulación. 
+     * @param saldo
+     */
+    public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+/**
+ * Metodo que nos retorna el tipo de interés que tiene la cuenta.
+ * Aquí se puede ver la encapsulación. 	
+ * @return
+ */
+    
+    public double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	
+/**
+ * Metodo que nos pide el tipo de interés.
+ * @param tipoInterés
+ */
+    public void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+
+/**
+ * 
+ * Metodo que nos retorna el saldo de la cuenta.
+ * Aquí se puede ver la encapsulación. 
+ * @return
+ */
+    
     public double estado()
     {
         return saldo;
     }
 
+/**
+ * Metodo que nos pide una cantidad de dinero para ingrasar a la cuenta.
+ * Tiene una excepcion de tipo Exception,la cual si el saldo es menor de nos manda un mensaje (No se puede ingresar una cantidad negativa).
+ * @param cantidad
+ * @throws Exception
+ */
+    
     public void ingresar(double cantidad) throws Exception
     {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
+        if (cantidad<0)//Si cantidad es menor que 0. 
+            throw new Exception("No se puede ingresar una cantidad negativa");//EXCEPCIÓN. 
         saldo = saldo + cantidad;
     }
 
+  /**
+   * Metodo que nos pide una cantidad de dinero para retirar de la cuenta.
+   * Tiene una excepcion de tipo Exception,la cual si la cantidad es menor e igual a 0 nos manda un mensaje (No se puede retirar una cantidad negativa). 
+   * Si  no se cumple la primeracondición pasara a la sengunda y nos mandara otro mensaje (No se hay suficiente saldo).
+   * @param cantidad
+   * @throws Exception
+   */
+    
     public void retirar(double cantidad) throws Exception
     {
-        if (cantidad <= 0)
+        if (cantidad <= 0)//Si cantidad es menor e igual que 0.
             throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+        if (estado()< cantidad)//Si el metodo estado es menor que cantidad.
+            throw new Exception ("No se hay suficiente saldo");//EXCEPCIÓN.
         saldo = saldo - cantidad;
     }
-    public void operativa_cuenta (String nom, String cue, double sal, double tipo)
-    {
-        
-    }
 }
+
